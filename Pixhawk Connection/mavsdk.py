0eed666c554a5aa41b3129e4ae0c9db3fc62e512
Bryan Acosta <70548114+bryan4027@users.noreply.github.com>
2023-04-03 18:26:08 -0500

    Create mavsdk.py

---
+++ Pixhawk Connection/mavsdk.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+from mavsdk.camera import (CameraError, Mode)
+from mavsdk import System
+
+
+async def run():
+    drone = System()
+    await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"-- Connected to drone!")
+            break
