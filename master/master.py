825174550d8363f8bd5e682f0bef37334ac61ee5
jeremykrill <jeremy.krill1@gmail.com>
2023-04-12 13:26:49 -0500

    initial integration of pixhawk data into master file

--- master/master.py
+++ master/master.py
@@ -12,6 +12,7 @@ import rasterio
 from crosscorr_screenshot import target_recognition
 from map import mapping
 from payload_algorithm import calculate_range, calculate_release_point, release_payload
+import pandas as pd
 
   
 if __name__ == "__main__":
@@ -19,18 +20,39 @@ if __name__ == "__main__":
     target_recognition()
 
     # mapping function
-    target_lat, target_long = mapping()
+    target_locations = mapping() # assumes this returns a list of target lats and longs
 
-    # payload algorithm
-    while True:
-        x = pixhawk.get_lat()
-        y = pixhawk.get_long()
-        v_x = pixhawk.get_vx()
-        v_y = pixhawk.get_vy()
-        v = np.sqrt(v_x**2 + v_y**2)
-        v_z = pixhawk.get_vz()
-        wind_speed = pixhawk.get_wind_speed()
-        altitude = pixhawk.get_altitude()
-        R = calculate_range(v, v_z, altitude, wind_speed)
-        release_point_lat, release_point_long = calculate_release_point(R, target_lat, target_long, x, y)
-        release_payload(R, x, y, target_lat, target_long, release_point_lat, release_point_long)
+    for i in range(len(target_locations)):
+        target_lat = target_locations[i][0]
+        target_long = target_locations[i][1]
+
+        # payload algorithm
+        while True:
+            mission_data = pd.read_csv('./../Pixhawk Connection/mission_callbacks/mission_callbacks.csv')
+            """ Old data format """
+            #critical_data = mission_data.drop(columns=['Global Location (relative altitude)', 'Local Location', 'Attitude', 'GPS'])
+            #critical_data[['lat', 'long', 'alt']] = mission_data['Global Location'].str.split(',', expand=True)
+            #critical_data['lat'] = critical_data['lat'].str.replace('LocationGlobal:lat=', '')
+            #critical_data['long'] = critical_data['long'].str.replace('lon=', '')
+            #critical_data['alt'] = critical_data['alt'].str.replace('alt=', '')
+            #critical_data = critical_data.drop(columns=['Global Location'])
+            #critical_data['Velocity'] = critical_data['Velocity'].apply(lambda x: x.strip('[]').split(','))
+
+            """ New data format """
+            # fieldnames = ['TIMESTAMP', 'yaw', 'pitch', 'roll', 'ground speed', 'air_speed', 'latitude', 'altitude', 'longitude', 'wind direction', 'wind speed', 'Heartbeat']
+            critical_data = mission_data.drop(columns=['yaw', 'pitch', 'roll', 'Heartbeat'])
+            critical_data['latitude'] = critical_data['latitude'].str.replace('lat=', '')
+            critical_data['longitude'] = critical_data['longitude'].str.replace('lon=', '')
+            critical_data['altitude'] = critical_data['altitude'].str.replace('alt=', '')
+
+            x = critical_data['latitude'].iloc[-1]
+            y = critical_data['longitude'].iloc[-1]
+            v = critical_data['ground_speed'].iloc[-1] # assuming single value
+            v_z = 0 # assuming 0
+            wind_speed = critical_data['wind speed'].iloc[-1] # assuming single value and head-on
+            # future work: depending on how wind_speed is recorded, we may need to account for wind direction if not head-on
+            altitude = critical_data['altitude'].iloc[-1]
+            time_delay = 2 # time delay in seconds
+            R = calculate_range(v, v_z, altitude, wind_speed, time_delay)
+            release_point_lat, release_point_long = calculate_release_point(R, target_lat, target_long, x, y)
+            release_payload(v, altitude, R, x, y, target_lat, target_long, release_point_lat, release_point_long)
--- master/payload_algorithm.py
+++ master/payload_algorithm.py
@@ -1,6 +1,7 @@
 import numpy as np
+from Pymavlink_movement import deploy_payload
 
-def calculate_range(v_x, v_z, H, wind_speed):
+def calculate_range(v_x, v_z, H, wind_speed, time_delay):
     rho = 1.225 # kg/m^3 for density of air at sea level
     C_d = 0.3 # just ballparking this, we should experimentally test it though
     A =  0.05 * 0.3 # guessing 10cm in diameter, 30cm in height
@@ -18,7 +19,6 @@ def calculate_range(v_x, v_z, H, wind_speed):
     x = 0
     z = 0
 
-    time_delay = 1 # assume 1 second time delay
     x = v_x * time_delay 
 
     terminal_velocity = np.sqrt((2*m*g)/(rho * A * C_d))
@@ -81,7 +81,7 @@ def release_payload_simulator(R, initial_lat, initial_long, v_x, v_y, target_lat
     for i in range(100000):
         x += v_x*h
         y += v_y*h
-        threshold = 0.05     # % error
+        threshold = 0.00     # % error
         if i % 100 == 0:
             #print("X IS: ", x)
             #print("Y IS: ", y)
@@ -94,17 +94,17 @@ def release_payload_simulator(R, initial_lat, initial_long, v_x, v_y, target_lat
         
     return x,y
 
-def release_payload(R, x, y, target_lat, target_long, release_point_lat, release_point_long):
+def release_payload(v, altitude, R, x, y, target_lat, target_long, release_point_lat, release_point_long):
     # if p_long ~ release_point_long and p_lat ~ release_point_lat, release payload
 
     r = [target_lat - x, target_long - y] # position vector from release point to target
     # return x,y when R is less than the distance between the release point and the target
     if np.linalg.norm(r) < R:
         print("RELEASE POINT IS NOT ON THE PATH")
-        send_pixhawk_signal()
+        deploy_payload(v, -1, release_point_lat, release_point_long, altitude)
 
-    threshold = 0.05 # % error
+    threshold = 0.02 # % error
     if ((1 - threshold)*release_point_lat <= abs(x) <= abs((1 + threshold)*release_point_lat)) and ((1 - threshold)*release_point_long <= abs(y) <= abs((1 + threshold)*release_point_long)):
         print("RELEASED WITHIN THRESHOLD")
-        send_pixhawk_signal()
+        deploy_payload(v, -1, release_point_lat, release_point_long, altitude)
         
